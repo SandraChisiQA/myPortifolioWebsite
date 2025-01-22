33a05c256341c8103e5c4c7c633bea588d9a44c6
sandra chisi <sandrachisi93@gmail.com>
2025-01-22 05:54:35 +0200

    I have added to my project a welcome message to address you according to the time of Day on your device

--- src/index.js
+++ src/index.js
@@ -14,11 +14,39 @@ function getButttonText(){
 }*/
 
 const App = () =>{
-
+   
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
     const buttonText = 'clickme:';
+
+    if (hours < 12) {
+    timeOfDay = "Morning"
+   }
+
+   else if(hours >= 12 && hours < 16){
+    timeOfDay = "Afternoon"
+   }
+
+   else if(hours >= 16 && hours < 20){
+    timeOfDay = "Evening"
+   }
+
+   else{
+    timeOfDay = "Night"
+   }
+
+
     return(
-        <div>
+        <div >
+            <h1 style={{color:"greenyellow"}} >
+                Good {timeOfDay} to you !!!
+            </h1>
+        
+
+        
             <h3>My first react project</h3>
+
             <label className="label" htmlFor="name">Enter name:</label>
             <input id="name" type="text" />
 
